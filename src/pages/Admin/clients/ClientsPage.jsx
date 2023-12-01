85968803a9eadf3c6f802c40d957fe6b385cff4b
morris menanya <emmiemenz@gmail.com>
2023-12-01 16:24:50 +0100

    update UI

--- src/pages/Admin/clients/ClientsPage.jsx
+++ src/pages/Admin/clients/ClientsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './client.css';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import default_photo from '../../../assets/users/profile_pic.jpg';
 import { listUsers } from '../../../redux/actions/users';
@@ -108,15 +108,44 @@ const ClientsPage = () => {
 
           : user.portfolios.map((portfolio) => (
             <div className="asset-infos">
-              <a href="" />
+              <li className='text-left box-shadow rounded-base my-1'>
+                <div className='flex rounded-base'>
+                    <div className='portfolio-img flex-1 p-3'>
+                        <img src={portfolio.asset.image_url} alt="" className='rounded-base' />
 
-              <div className="asset-info flex-space">
-                <span>{portfolio.asset.name}</span>
-                <span>
-                  $
-                  {portfolio.amount}
-                </span>
-              </div>
+                    </div>
+                    <div className='flex-2 p-3 '>
+                        <div className='flex '>
+
+                      
+                            <div className='flex-1' >
+                        
+                            <h4>{portfolio.asset.name}</h4>
+                            <p><i class="fa fa-map-marker icon"></i>{portfolio.asset.address}</p>
+                                
+                            </div>
+                            <div><span className='text-lg font-medium text-green' >Profits: </span> <span className='text-lg font-medium'>{usd_format(portfolio.investment_interest)}</span></div>
+                        </div>
+                        <div className='flex justify-between'>
+                            <div className='flex-1'>
+                          
+                             <span className='text-xl font-semibold'>{portfolio.asset.price}</span>
+      
+                            </div>
+                            <div className='flex-1 flex justify-between'>
+                                <span><i class="fa fa-bed"></i> {portfolio.asset.number_of_bedrooms} beds</span>
+                                <span><i class="fa fa-tint"></i> {portfolio.asset.number_of_bathrooms} Baths</span>
+                                <span><i class="fa fa-expand"></i> {portfolio.asset.area}Sq Ft</span>
+                            </div>
+                        </div>
+                        <div class="text-right p-2">
+                            <NavLink to="" className="btn inline-block p-1 text-white">view interest</NavLink>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </li>
 
             </div>
           ))}
