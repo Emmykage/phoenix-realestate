c747318fe0d51c97785287b5beee60723365a3b8
morris menanya <emmiemenz@gmail.com>
2025-05-13 03:18:54 +0100

    update

--- src/redux/baseURL.js
+++ src/redux/baseURL.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000/api/v1/"
-// const baseUrl = "https://phoenix-api-b176bc85b068.herokuapp.com/api/v1/"
+// const baseUrl = "http://localhost:3000/api/v1/"
+const baseUrl = "https://phoenix-api-b176bc85b068.herokuapp.com/api/v1/"
 
 export default baseUrl
